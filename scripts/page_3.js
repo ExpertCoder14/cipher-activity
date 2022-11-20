357d1257143b74003cc60e4ffee43ed75c6f6018
ExpertCoder14 <61667233+ExpertCoder14@users.noreply.github.com>
2022-11-19 17:29:02 -0800

    Add size change functionality to cipher 3

--- scripts/page_3.js
+++ scripts/page_3.js
@@ -1,5 +1,7 @@
 // Enter the message to encrypt. Use `\n` to break lines.
 let message = 'IT IS\nGUARDED\nBY A\nGIANT\nTOAD';
+// Enter a size for the symbols. 1 creates symbols with side length 1in.
+let size = 0.5;
 
 // MODIFY VARIABLES ABOVE THIS LINE
 
@@ -16,7 +18,7 @@ message.forEach((line, lineNum) => {
 			// Create a new element for the symbol and add it to the page
 			let symbol = document.createElementNS(svgNS, 'use');
 			symbol.setAttribute('href', '#pigpen-' + line.charAt(i).toLowerCase());
-			symbol.setAttribute('transform', 'scale(0.5) translate(' + x + ', ' + lineNum * 1.5 + ')');
+			symbol.setAttribute('transform', 'scale(' + size + ') translate(' + x + ', ' + lineNum * 1.5 + ')');
 			document.querySelector('#message').appendChild(symbol);
 			x += 1.5;
 		}
